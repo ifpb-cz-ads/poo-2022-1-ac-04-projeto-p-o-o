9a144f4ec27ed89cfb23e9f766fb7f5af6d454d0
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2022-06-01 23:01:21 -0300

    CBBAtividadePratica5.java
    Questão 5 Baruque

---
+++ Par.java
@@ -0,0 +1,10 @@
+public class Par{   
+public static void main(String args[]){
+int valor;
+    for(int i=0;i<=100; i++){
+if (i % 2 == 0){
+    System.out.println("i = " + i);
+         }
+      }
+   }
+}
